8c98395c94fb4fd63fb5c448bf8af1359aecb125
민경호 <1736s@naver.com>
2024-07-10 14:20:49 +0900

    feat: generatedUniqueId 수정
    - window.crypto.randomUUID 사용

--- src/lib/generatedUniqueId.ts
+++ src/lib/generatedUniqueId.ts
@@ -1,3 +1,7 @@
 export const generateUniqueId = () => {
-  return "_" + String(Math.random().toString(36).slice(2, 11));
+  if (typeof window === "undefined") {
+    return "_" + String(Math.random().toString(36).slice(2, 11));
+  }
+
+  return "_" + window.crypto.randomUUID();
 };
