9b9b949c781d8f4162b8777bcefc3731961c2fae
Sebin-Myung <wrqewrq213@naver.com>
2024-07-12 09:47:03 +0900

    feat: tooltip event 적용

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { TooltipWrapper, Tri, Wrapper } from "./Tooltip.style";
 import { TooltipProps } from "./Tooltip.type";
 import { generateUniqueId } from "../../lib";
@@ -12,19 +12,19 @@ const Tooltip = ({
   triPosition = "center",
   color = "black",
   content,
-}: // disabled = false,
-TooltipProps) => {
+  disabled = false,
+}: TooltipProps) => {
   // refs
   const triggerRef = useRef<HTMLElement>(null);
   const tooltipRef = useRef<HTMLDivElement>(null);
 
   // tooltip portal
   const [container, setContainer] = useState<Element | null>(null);
-  const isOpen = container !== null;
   const uniqueId = useMemo(() => generateUniqueId(), []);
 
   // open/close
   const open = () => {
+    if (disabled) return;
     const newContainer = document.createElement("div");
     newContainer.id = uniqueId;
     document.body.appendChild(newContainer);
@@ -35,31 +35,16 @@ TooltipProps) => {
     container?.remove();
     setContainer(null);
   };
-  const toggle = () => {
-    isOpen ? close() : open();
-  };
 
   // tooltip의 위치를 정하는 로직
   useTooltipPosition({ container, triggerRef, tooltipRef, position, triPosition });
-  // event handlers
-  useEffect(() => {
-    toggle();
-  }, []);
-
-  // trigger
-  const [trigger, setTrigger] = useState<React.ReactNode>();
-  useEffect(() => {
-    setTrigger(
-      React.cloneElement(children, {
-        ...children.props,
-        ref: triggerRef,
-      })
-    );
-  }, []);
 
   return (
-    <Wrapper>
-      {trigger}
+    <Wrapper onMouseEnter={open} onMouseLeave={close} onFocus={open} onBlur={close}>
+      {React.cloneElement(children, {
+        ...children.props,
+        ref: triggerRef,
+      })}
       <Portal container={container}>
         <TooltipWrapper role="tooltip" ref={tooltipRef} $position={position} $triPosition={triPosition} $color={color}>
           {content}
