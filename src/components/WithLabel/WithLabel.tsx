b80e2555e138f39b33b37f45c16e6c426022d4e9
Sebin-Myung <wrqewrq213@naver.com>
2024-07-25 13:33:27 +0900

    feat: withLabel tag 수정

--- src/components/WithLabel/WithLabel.tsx
+++ src/components/WithLabel/WithLabel.tsx
@@ -2,16 +2,12 @@ import Text from "../Text";
 import { BaseLabel } from "./WithLabel.style";
 import { WithLabelProps } from "./WithLabel.type";
 
-const WithLabel = ({
-  required = false,
-  label,
-  children,
-  typo = "caption12Medium",
-  gap = 2,
-}: WithLabelProps) => {
+const WithLabel = ({ required = false, label, children, typo = "caption12Medium", gap = 2 }: WithLabelProps) => {
   return (
     <BaseLabel data-required={required} style={{ gap }}>
-      <Text typo={typo}>{label}</Text>
+      <Text typo={typo} as="label">
+        {label}
+      </Text>
       {children}
     </BaseLabel>
   );
