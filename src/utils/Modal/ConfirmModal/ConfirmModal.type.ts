2ee555313f8f6ecf8556884b436686db9c8947ea
민경호 <1736s@naver.com>
2025-02-13 16:01:45 +0900

    feat: confirm modal의 cancelVariant에 secondary 추가

--- src/utils/Modal/ConfirmModal/ConfirmModal.type.ts
+++ src/utils/Modal/ConfirmModal/ConfirmModal.type.ts
@@ -5,7 +5,7 @@ export type ConfirmModalProps = {
   onCancel?: () => Promise<void>;
   confirmLabel?: string;
   cancelLabel?: string;
-  cancelVariant?: "gray" | "black";
+  cancelVariant?: "secondary" | "gray" | "black";
   confirmVariant?: "primary" | "black";
   preventConfirmClose?: boolean; // onConfirm 함수가 실행된 후 모달을 닫을지 여부
   disabled?: boolean;
