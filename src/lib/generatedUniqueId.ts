ad41b6870e35045f329694c3a7312e6fd3cfb6f7
민경호 <1736s@naver.com>
2025-03-31 11:16:09 +0900

    fix: window.crypyo.randomUUID()를 try-catch로 감싸기

--- src/lib/generatedUniqueId.ts
+++ src/lib/generatedUniqueId.ts
@@ -1,7 +1,12 @@
 export const generateUniqueId = () => {
-  if (typeof window === "undefined") {
+  try {
+    if (typeof window === "undefined") {
+      return "_" + String(Math.random().toString(36).slice(2, 11));
+    }
+
+    return "_" + window.crypto.randomUUID();
+  } catch (e) {
+    console.error(e);
     return "_" + String(Math.random().toString(36).slice(2, 11));
   }
-
-  return "_" + window.crypto.randomUUID();
 };
